ad849a9286ef09ef99aa70ee28e0e655024d65b8
Mustafa Burak Aydın <mburak.aydin@xenonsmart.com>
2024-08-04 09:42:58 +0000

    added package class-validator class-transformer

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import userRoutes from "./routes/user.route";
 import bookRoutes from "./routes/book.route";
-import borrowRoutes from "./routes/borrow-book.route";
 
 const app = express();
 
@@ -9,6 +8,5 @@ app.use(express.json());
 
 app.use(userRoutes);
 app.use(bookRoutes);
-app.use(borrowRoutes);
 
 export default app;
--- src/routes/borrow-book.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { borrowBook, returnBook } from "../controllers/borrow-book.controller";
-
-const router = Router();
-
-router.post("/borrow", borrowBook);
-router.post("/return", returnBook);
-
-export default router;
